8d7bc1dc80bda99ab7ba5e3cfcf028178ec6a91e
Nolan Marks <foo@bar.com>
2020-07-03 22:57:12 +0100

    nodeify

--- flesch-kincaid.js
+++ flesch-kincaid.js
@@ -1,12 +1,4 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-/*eslint prefer-const: "error", sourceType: "module" */
-/*eslint-env es6*/
-
-var syllables = function syllables(x) {
+const syllables = x => {
     /*
      * basic algortithm: each vowel-group indicates a syllable, except for: final
      * (silent) e 'ia' ind two syl @AddSyl and @SubSyl list regexps to massage the
@@ -14,18 +6,36 @@ var syllables = function syllables(x) {
      * @SubSyl match -1 Keep in mind that when the regexps are checked, any final
      * 'e' will have been removed, and all '\'' will have been removed.
      */
-    var subSyl = [/cial/, /tia/, /cius/, /cious/, /giu/, // belgium!
-    /ion/, /iou/, /sia$/, /.ely$/, // absolutely! (but not ely!)
-    /sed$/];
+    const subSyl = [
+        /cial/,
+        /tia/,
+        /cius/,
+        /cious/,
+        /giu/, // belgium!
+        /ion/,
+        /iou/,
+        /sia$/,
+        /.ely$/, // absolutely! (but not ely!)
+        /sed$/, // doused, housed, used
+    ]
 
-    var addSyl = [/ia/, /riet/, /dien/, /iu/, /io/, /ii/, /[aeiouym]bl$/, // -Vble, plus -mble
-    /[aeiou]{3}/, // agreeable
-    /^mc/, /ism$/, // -isms
-    /([^aeiouy])\1l$/, // middle twiddle battle bottle, etc.
-    /[^l]lien/, // // alien, salient [1]
-    /^coa[dglx]./, // [2]
-    /[^gq]ua[^auieo]/, // i think this fixes more than it breaks
-    /dnt$/];
+    const addSyl = [
+        /ia/,
+        /riet/,
+        /dien/,
+        /iu/,
+        /io/,
+        /ii/,
+        /[aeiouym]bl$/, // -Vble, plus -mble
+        /[aeiou]{3}/, // agreeable
+        /^mc/,
+        /ism$/, // -isms
+        /([^aeiouy])\1l$/, // middle twiddle battle bottle, etc.
+        /[^l]lien/, // // alien, salient [1]
+        /^coa[dglx]./, // [2]
+        /[^gq]ua[^auieo]/, // i think this fixes more than it breaks
+        /dnt$/, // couldn't
+    ]
 
     // (comments refer to titan's /usr/dict/words)
     // [1] alien, salient, but not lien or ebbullient...
@@ -33,47 +43,25 @@ var syllables = function syllables(x) {
     // [2] exception for 7 words:
     // coadjutor coagulable coagulate coalesce coalescent coalition coaxial
 
-    var xx = x.toLowerCase().replace(/'/g, '').replace(/e\b/g, '');
-    var scrugg = xx.split(/[^aeiouy]+/).filter(Boolean); // '-' should be perhaps added?
+    const xx = x.toLowerCase().replace(/'/g, '').replace(/e\b/g, '')
+    const scrugg = xx.split(/[^aeiouy]+/).filter(Boolean) // '-' should be perhaps added?
 
-    return undefined === x || null === x || '' === x ? 0 : 1 === xx.length ? 1 : subSyl.map(function (r) {
-        return (xx.match(r) || []).length;
-    }).reduce(function (a, b) {
-        return a - b;
-    }) + addSyl.map(function (r) {
-        return (xx.match(r) || []).length;
-    }).reduce(function (a, b) {
-        return a + b;
-    }) + scrugg.length - (scrugg.length > 0 && '' === scrugg[0] ? 1 : 0) +
-    // got no vowels? ("the", "crwth")
-    xx.split(/\b/).map(function (x) {
-        return x.trim();
-    }).filter(Boolean).filter(function (x) {
-        return !x.match(/[.,'!?]/g);
-    }).map(function (x) {
-        return x.match(/[aeiouy]/) ? 0 : 1;
-    }).reduce(function (a, b) {
-        return a + b;
-    });
-};
+    return (undefined === x || null === x || '' === x) ? 0 :
+           (1 === xx.length) ? 1 :
+           subSyl.map(r => (xx.match(r) || []).length).reduce((a, b) => a - b) +
+           addSyl.map(r => (xx.match(r) || []).length).reduce((a, b) => a + b) +
+           scrugg.length - ((scrugg.length > 0 && '' === scrugg[0]) ? 1 : 0) +
+           // got no vowels? ("the", "crwth")
+           xx.split(/\b/).map(x => x.trim()).filter(Boolean).filter(x => !x.match(/[.,'!?]/g)).map(x => x.match(/[aeiouy]/) ? 0 : 1).reduce((a, b) => a + b)
 
-var words = function words(x) {
-    return (x.split(/\s+/) || ['']).length;
-};
-var sentences = function sentences(x) {
-    return (x.split('. ') || ['']).length;
-};
-var syllablesPerWord = function syllablesPerWord(x) {
-    return syllables(x) / words(x);
-};
-var wordsPerSentence = function wordsPerSentence(x) {
-    return words(x) / sentences(x);
-};
+}
 
-var rate = exports.rate = function rate(x) {
-    return 206.835 - 1.015 * wordsPerSentence(x) - 84.6 * syllablesPerWord(x);
-};
-var grade = exports.grade = function grade(x) {
-    return 0.39 * wordsPerSentence(x) + 11.8 * syllablesPerWord(x) - 15.59;
-};
+const words = x => (x.split(/\s+/) || ['']).length
+const sentences = x => (x.split('. ') || ['']).length
+const syllablesPerWord = x => syllables(x) / words(x)
+const wordsPerSentence = x => words(x) / sentences(x)
 
+module.export = {
+    rate: x => 206.835 - 1.015 * wordsPerSentence(x) - 84.6 * syllablesPerWord(x),
+    grade: x => 0.39 * wordsPerSentence(x) + 11.8 * syllablesPerWord(x) - 15.59
+};
